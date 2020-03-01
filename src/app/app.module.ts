712789ef826a7df0be03c5f48d309fa5f96a8339
Nitai Charan <nitaicharan@gmail.com>
2020-03-01 00:00:00 -0300

    32. Using Schematics for Actions, Reducers, and Containers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,42 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
-import { customersReducer } from './customers/customer.reducer';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-
-
-
-
-
-// PRE-STEP: npm install @ngrx/store @ngrx/store-devtools
-
-// Sets up root store, reducers and meta reducers
-// ng generate @ngrx/schematics:store State --root --module app.module.ts
-
-/* create Admin module, add feature store setup and basic reducers and meta reducers 
-ng generate module admin --flat false
-ng generate @ngrx/schematics:store admin/Admin -m admin.module.ts
-*/
-
-// ng generate @ngrx/schematics:store admin/Admin -m admin.module.ts
-
