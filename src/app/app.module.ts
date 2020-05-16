73118f00559fd88e30128268bae179d5ba250cf3
Nitai Charan <nitaicharan@gmail.com>
2020-05-16 14:47:49 -0300

    21. Advanced Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './products/products.module';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+const actionSanitizer = (action) => Object.assign({ ...action, payload: 'hiding the payload' })
+const stateSanitizer = (state, index) => Object.assign({ ...state, list: ['hiding the payload'] })
+
+const devInstrument = { maxAge: 20, name: 'my DEV instance' }
+const prodInstrument = { maxAge: 20, name: 'my PROD instance', actionSanitizer, stateSanitizer }
 
 @NgModule({
   declarations: [
@@ -17,7 +24,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     FormsModule,
     ProductModule,
     StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument({ maxAge: 25 }),
+    StoreDevtoolsModule.instrument(environment.production ? prodInstrument : devInstrument),
   ],
   providers: [],
   bootstrap: [AppComponent]
